5ac16450818ce43f512690958e7a0d08cf9177cb
yashj09 <yashj8858@gmail.com>
2024-01-10 00:09:24 +0530

    added links

--- src/Pages/UserOutput.tsx
+++ src/Pages/UserOutput.tsx
@@ -7,31 +7,41 @@ const UserOutput = () => {
           <div className="image">
             <img src="user.jpg" alt="User-Image" />
           </div>
-          <a href="https://x.com">
-            <div className="card">
-              Twitter
-            </div>
-          </a>
-          <a href="https://instagram.com">
-            <div className="card">
-              Instagram
-            </div>
-          </a>
-          <a href="https://github.com">
-            <div className="card">
-              Github
-            </div>
-          </a>
-          <a href="https://linkdln.com">
-            <div className="card">
-              Linkdln
-            </div>
-          </a>
-          <a href="https://reddit.com">
-            <div className="card">
-              Reddit
-            </div>
-          </a>
+          <div className="links">
+            <a href="https://x.com" target="_blank">
+              <div className="card">
+                Twitter
+              </div>
+            </a>
+          </div>
+          <div className="links">
+            <a href="https://instagram.com" target="_blank">
+              <div className="card">
+                Instagram
+              </div>
+            </a>
+          </div>
+          <div className="links">
+            <a href="https://github.com" target="_blank">
+              <div className="card">
+                Github
+              </div>
+            </a>
+          </div>
+          <div className="links">
+            <a href="https://www.linkedin.com/" target="_blank">
+              <div className="card">
+                Linkedin
+              </div>
+            </a>
+          </div>
+          <div className="links">
+            <a href="https://reddit.com" target="_blank">
+              <div className="card">
+                Reddit
+              </div>
+            </a>
+          </div>
         </div>
       </div>
     </>
