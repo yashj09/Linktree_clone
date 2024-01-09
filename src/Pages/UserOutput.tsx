2ba68dbeacee9fd95ab4ab8ac7903a73c66aa682
yashj09 <yashj8858@gmail.com>
2024-01-09 23:34:47 +0530

    output

--- src/Pages/UserOutput.tsx
+++ src/Pages/UserOutput.tsx
@@ -1,7 +1,27 @@
 
 const UserOutput = () => {
   return (
-    <div>UserOutput</div>
+    <>
+      <div className="contain">
+        <div className="centrebox">
+          <div className="card">
+            Twitter
+          </div>
+          <div className="card">
+            Instagram
+          </div>
+          <div className="card">
+            Github
+          </div>
+          <div className="card">
+            Linkdln
+          </div>
+          <div className="card">
+            Reddit
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
