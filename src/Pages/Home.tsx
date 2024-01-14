adab5981726457a311dcba447ce02663cb130dae
yashj09 <yashj8858@gmail.com>
2024-01-14 23:59:42 +0530

    added navigation

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,12 +1,18 @@
 import "./home.css"
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate();
+  const handle = () => {
+    navigate(`/UserInfo`)
+  }
   return (
     <>
       <div className="name">
         <div className="text">
           <span>Linktree.com/</span>
-          <input type="text" placeholder="Enter your name" />
+          <input type="text" placeholder="Enter your name" onChange={handle} />
+
         </div>
       </div>
     </>
